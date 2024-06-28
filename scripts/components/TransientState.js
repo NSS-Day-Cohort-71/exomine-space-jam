96cc6fb01091683f5f5cecdeab928ed665490565
Jeremy Whitney <bearwhitness@gmail.com>
2024-06-28 14:49:03 -0500

    created transient state data structure and setter functions

--- scripts/components/TransientState.js
+++ scripts/components/TransientState.js
@@ -1,12 +1,33 @@
-const state = {
-
+const transientState = {
+    "governorId": 0,
+    "facilityId": 0,
+    "colonyId": 0,
+    "mineralId": 0,
+    "amount": 0,
 }
 
-export const setFacility = (facilityId) => {
-    state.selectedFacility = facilityId
-    document.dispatchEvent(new CustomEvent("stateChanged"))
+export const setGovernor = (chosenGovernor) => {
+    transientState.governorId = chosenGovernor
+    console.log(transientState)
 }
 
+export const setFacility = (chosenFacility) => {
+    transientState.facilityId = chosenFacility
+    console.log(transientState)
+    // document.dispatchEvent(new CustomEvent("stateChanged")) // Pre-loaded with repo
+}
+
+export const setColony = (chosenColony) => {
+    transientState.colonyId = chosenColony
+    console.log(transientState)
+}
+
+export const setMineral = (chosenMineral) => {
+    transientState.mineralId = chosenMineral
+    console.log(transientState)
+}
+
+// Also pre-loaded wtih repo. This *should* be our PUT function for this module.
 export const purchaseMineral = () => {
     /*
         Does the chosen governor's colony already own some of this mineral?
