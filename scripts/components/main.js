27114ef07de0cfa3fbbdb40d4fa473b6efdf261f
Jeremy Whitney <bearwhitness@gmail.com>
2024-07-03 14:25:45 -0500

    Rough CSS layout

--- scripts/components/main.js
+++ scripts/components/main.js
@@ -10,40 +10,45 @@ const render = async () => {
 
     const theHTML = `
         <h1>Solar System Mining Marketplace</h1>
-
-        <section id="governors__list">
-            <div>Choose a governor
-                <select id="governorDropdown">
-                    <option disabled selected value="">Choose a governor...</option>
-                </select>
-            </div>
-        </section>
-
-        <section id="colonyDetails">
-            <h2>Colony Minerals</h2>
-        </section>
-
-        <section id="facility">
-            <div id="facilityDropdown">
-                ${facilityHTML}
-            </div>
-        </section>
-
-        <section id="facilityDetails">
-            <div id="mineralsList">
-                <h2 id="facilityMineralsTitle">Facility Minerals</h2>
-            </div>
-        </section>
-
-        <section id="theSpaceCartSection">
-            <h1>Space Cart</h1>
-            <div id="spaceCartButtonContainer">
-                <div id="spaceCart">
-                
+        <div id="mainContent">
+            <section id="governors__list">
+                <div>Choose a governor
+                    <select id="governorDropdown">
+                        <option disabled selected value="">Choose a governor...</option>
+                    </select>
                 </div>
-                ${placeOrder}
-            </div>
-        </section>
+            </section>
+
+            <section id="colonyDetails">
+                <h2>Colony Minerals</h2>
+            </section>
+        </div>
+
+        <div id="mainContent">
+            <section id="facility">
+                <div id="facilityDropdown">
+                    ${facilityHTML}
+                </div>
+            </section>
+        </div>
+
+        <div id="lowerContent">
+            <section id="facilityDetails">
+                <div id="mineralsList">
+                    <h2 id="facilityMineralsTitle">Facility Minerals</h2>
+                </div>
+            </section>
+
+            <section id="theSpaceCartSection">
+                <h2>Space Cart</h2>
+                <div id="spaceCart">
+
+                </div>
+                <div id="spaceCartButtonContainer">
+                    ${placeOrder}
+                </div>
+            </section>
+        </div>
     `
 
     container.innerHTML = theHTML
