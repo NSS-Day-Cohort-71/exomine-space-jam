e73ac9cf77c319504e93820eacb3ed734a2b41e5
Lane Dillard <lane.holland04@gmail.com>
2024-07-03 09:11:35 -0500

    updated

--- scripts/components/TransientState.js
+++ scripts/components/TransientState.js
@@ -30,66 +30,7 @@ export const setMineral = (chosenMineral) => {
     console.log(transientState)
 }
 
-// Also pre-loaded wtih repo. This *should* be our PUT function for this module.
-// export const purchaseMineral = async () => {
-    /*
-        Does the chosen governor's colony already own some of this mineral?
-            - If yes, what should happen?
-            - If no, what should happen?
 
-        Defining the algorithm for this method is traditionally the hardest
-        task for teams during this group project. It will determine when you
-        should use the method of POST, and when you should use PUT.
-
-        Only the foolhardy try to solve this problem with code.
-    */
-//     const colonyMineralResponse = await fetch("http://localhost:8088/colonyMinerals?_expand=mineral&_expand=colony")
-//     const colonyMineral = await colonyMineralResponse.json()
-//     const facilityMineralResponse = await fetch("http://localhost:8088/facilityMinerals?_expand=mineral&_expand=facility")
-//     const facilityMineral = await facilityMineralResponse.json()
-
-    
-    
-
-//     const facility = facilityMineral.find(fm => fm.facilityId === transientState.facilityId)?.facility
-//     const matchedMinerals = facilityMineral.filter(fm => fm.mineralId === transientState.mineralId || fm.facilityId === transientState.facilityId);
-
-//     const carl = facilityMineral.filter (fm => fm.id === transientState.mineralId)
-
-
-//     const karl = colonyMineral.filter (cm => cm.id === transientState.colonyId)
-
-    
-
-//     const facilityOptions = {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type' : 'application/json'
-//         },
-//         body: JSON.stringify({
-//             ...carl,
-//             quantity: carl.quantity - 1 // Subtract 1 from facility mineral
-//         })
-//     };
-//     const facilityUpdating = await fetch("http://localhost:8088/facilityMinerals?_expand=mineral&_expand=facility", facilityOptions)
-//     console.log(facilityOptions.body)
-
-//     const colonyOptions = {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type' : 'application/json'
-//         },
-//         body: JSON.stringify({
-//             ...karl,
-//             quantity: karl.quantity + 1
-//         })
-//     };
-//     const colonyUpdating = await fetch("http://localhost:8088/colonyMinerals?_expand=mineral&_expand=colony", colonyOptions)
-    
-//     console.log(colonyOptions.body)
-
-//     document.dispatchEvent(new CustomEvent("stateChanged"))
-// }
 
 export const purchaseMineral = async () => {
     // Fetch colony minerals
