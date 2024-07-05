172df4591b01aabf8f4847edc0a1af86406b6336
Jeremy Whitney <bearwhitness@gmail.com>
2024-07-05 13:47:09 -0500

    Disabled purchase mineral button for inactive facilities

--- scripts/components/facilityList.js
+++ scripts/components/facilityList.js
@@ -8,6 +8,9 @@ const handleFacilityChoice = async (event) => {
         const facilityId = parseInt(event.target.value)
         setFacility(facilityId) // Update transient state
 
+        const facilitiesData = await facilities();
+        const selectedFacility = facilitiesData.find(facility => facility.id === facilityId);
+
         // Update minerals list for the selected facility
         const mineralsHTML = await facilityMineralOptions(facilityId)
         document.getElementById("mineralsList").innerHTML = mineralsHTML
@@ -23,6 +26,13 @@ const handleFacilityChoice = async (event) => {
 
         // Set the selected facility in the dropdown
         document.querySelector('select[name="facility"]').value = facilityId.toString()
+
+        // Disable the purchase button if facility is inactive
+        if (!selectedFacility.status) {
+            document.getElementById("placeOrder").disabled = true;
+        } else {
+            document.getElementById("placeOrder").disabled = false;
+        }
     }
 }
 
