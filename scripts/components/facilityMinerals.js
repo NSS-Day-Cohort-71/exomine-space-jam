93743cd352f6aef37b1b2afc1a1e768d9b99c1f6
Jeremy Whitney <bearwhitness@gmail.com>
2024-07-05 19:04:38 -0500

    Remove radio button for minerals with qty 0

--- scripts/components/facilityMinerals.js
+++ scripts/components/facilityMinerals.js
@@ -12,21 +12,36 @@ export const facilityMineralOptions = async (facilityId) => {
     const facilityMinerals = await response.json();
 
     const facility = facilityMinerals.find(fm => fm.facilityId === facilityId)?.facility
-    const matchedMinerals = facilityMinerals.filter(fm => fm.facilityId === facilityId);
+    const matchedMinerals = facilityMinerals.filter(fm => fm.facilityId === facilityId );
     
     let mineralsHTML = `<h2 id="facilityMineralsTitle">Facility Minerals` 
     if (facility) {
         mineralsHTML += ` for ${facility.name}`;
     }
     mineralsHTML += `</h2>`
+
     matchedMinerals.forEach(fm => {
-        mineralsHTML += `<div><input type='radio' value='${fm.mineral.id}' name="mineral">${fm.quantity} tons of ${fm.mineral.type}</input></div>`;
-    });
+        if (fm.quantity > 0) {
+            mineralsHTML += `<div><input type='radio' value='${fm.mineral.id}' name="mineral">${fm.quantity} tons of ${fm.mineral.type}</input></div>`
+        } else {
+            mineralsHTML += `<div>${fm.quantity} tons of ${fm.mineral.type}</div>`
+        }
+    })
     
     document.addEventListener("change", handleMineralChoice)
 
     const mineralsContainer = document.getElementById("mineralsList");
     mineralsContainer.innerHTML = mineralsHTML;
+
+    // Remove radio buttons for minerals with quantity 0
+    matchedMinerals.forEach(fm => {
+        if (fm.quantity === 0) {
+            const radio = document.querySelector(`input[type='radio'][value='${fm.mineral.id}']`)
+            if (radio) {
+                radio.parentNode.removeChild(radio)
+            }
+        }
+    })
 
     return mineralsHTML;
 };
